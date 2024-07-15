4e3b572afe8d0e334ba7bf52d3ae8fe69ebdf3fc
Guilherme Rolemberg <guilherme.rolemberg@outlook.com>
2024-07-15 20:21:20 -0300

    simple validator

--- Native.Application/Validators/CreateSneakerCommandvalidator.cs
+++ Native.Application/Validators/CreateSneakerCommandvalidator.cs
@@ -6,13 +6,41 @@ public class CreateSneakerCommandvalidator : AbstractValidator<CreateSneakerComm
 {
     public CreateSneakerCommandvalidator()
     {
+        RuleFor(p => p.IdUser)
+            .NotEmpty()
+            .WithMessage("IdUser is required");
+
+        RuleFor(p => p.Name)
+            .NotEmpty()
+            .WithMessage("Name is required");
+
         RuleFor(p => p.Name)
             .MaximumLength(255)
             .WithMessage("Maximum length for Name field is 255 characters.");
 
         RuleFor(p => p.Brand)
+            .NotEmpty()
+            .WithMessage("Brand is required");
+
+        RuleFor(p => p.Brand)
             .MaximumLength(30)
             .WithMessage("Maximum length for Brand field is 30 characters.");
+
+        RuleFor(p => p.Price)
+            .NotEmpty()
+            .WithMessage("Price is required");
+
+        RuleFor(p => p.Size)
+            .NotEmpty()
+            .WithMessage("Size is required");
+
+        RuleFor(p => p.Year)
+            .NotEmpty()
+            .WithMessage("Year is required");
+
+        RuleFor(p => p.Rate)
+            .NotEmpty()
+            .WithMessage("Rate is required");
 
         RuleFor(p => p.Rate)
             .InclusiveBetween(1, 4)
--- Native.Application/Validators/CreateUserCommandValidator.cs
+++ Native.Application/Validators/CreateUserCommandValidator.cs
@@ -8,15 +8,23 @@ public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
     public CreateUserCommandValidator()
     {
         RuleFor(p => p.Email)
+            .NotEmpty()
+            .WithMessage("Email is required");
+
+        RuleFor(p => p.Email)
             .EmailAddress()
-            .WithMessage("E-mail invalid!");
+            .WithMessage("Email is not valid");
+
+        RuleFor(p => p.Password)
+            .NotEmpty()
+            .WithMessage("Password is required");
 
         RuleFor(p => p.Password)
             .Must(ValidPassword)
             .WithMessage("Password must contain at least 8 characters, a number, an uppercase letter, a lowercase letter, and a special character");
     }
 
-    public static bool ValidPassword(string password)
+    private static bool ValidPassword(string password)
     {
         var regex = new Regex(@"^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$");
 
